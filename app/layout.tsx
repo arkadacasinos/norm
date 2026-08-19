010aa57d6dc53e6ac48b4fd61ef8327a89d7963f
arkadacasinos <bezdep.rf@mail.ru>
2026-08-20 02:35:30 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from 'next'
 import './globals.css'
 
-const siteUrl = 'https://evacasino83.vercel.app/'
+const siteUrl = 'https://normcasino.vercel.app/'
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
